dc5ebf3c72f2cf916e45b60129ab59619359c5d8
myFirst <shwetapjadhav12@gmail.com>
2025-06-27 22:20:35 +0530

    quick fix

--- app/src/main/java/com/example/project_2_ecommerce_app/screen/HomeScreen.kt
+++ app/src/main/java/com/example/project_2_ecommerce_app/screen/HomeScreen.kt
@@ -1,7 +1,5 @@
 package com.example.project_2_ecommerce_app.screen
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -10,7 +8,6 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -19,8 +16,7 @@ import androidx.navigation.NavController
 import com.example.project_2_ecommerce_app.PAGES.CartPage
 import com.example.project_2_ecommerce_app.PAGES.FavoritePage
 import com.example.project_2_ecommerce_app.PAGES.HomePage
-import com.example.project_2_ecommerce_app.PAGES.ProfilePage
-import com.google.firebase.auth.FirebaseAuth
+import com.example.project_2_ecommerce_app.pages.ProfilePage
 
 @Composable
 fun HomeScreen(navController: NavController) {
